e898599954c5e9d2af47420b50199c3fc2062af4
hoangminh <minhdev2017@gmail.com>
2021-07-11 08:06:00 +0700

    a

--- lib/sliding_up_panel.dart
+++ lib/sliding_up_panel.dart
@@ -1,6 +1,6 @@
 /*
 Name: Akshath Jain
-Date: 3/30/19
+Date: 3/30/2021
 Purpose: Defines the package: sliding_up_panel
 Copyright: © 2019, Akshath Jain. All rights reserved.
 Licensing: More information can be found here: https://github.com/akshathjain/sliding_up_panel/blob/master/LICENSE
